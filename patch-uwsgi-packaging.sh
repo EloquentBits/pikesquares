ec6d4cd35fe7a0250fcac28df5152e28f655a2bb
Philip Kalinsky <philip.kalinsky@eloquentbits.com>
2023-12-26 08:46:21 -0500

    patch uwsgi packaging script

---
+++ patch-uwsgi-packaging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+SRC=$1
+APPEND_VERSION=${APPEND_VERSION:-""}
+
+cp setup.py "$SRC"/setup.py
+sed -i.bak 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' $SRC/setup.py
+rm "$SRC"/setup.py.bak
+rm "$SRC"/PKG-INFO
