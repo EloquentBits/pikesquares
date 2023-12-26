c6a9a4b36980bb2578dd2af5f751c38416aebebb
Philip Kalinsky <philip.kalinsky@eloquentbits.com>
2023-12-26 09:19:00 -0500

    actions testing

--- patch-uwsgi-packaging.sh
+++ patch-uwsgi-packaging.sh
@@ -8,4 +8,4 @@ APPEND_VERSION=${APPEND_VERSION:-""}
 cp setup.py "$SRC"/setup.py
 sed -i.bak 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' $SRC/setup.py
 rm "$SRC"/setup.py.bak
-rm "$SRC"/PKG-INFO
+#rm "$SRC"/PKG-INFO
