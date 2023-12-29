6003653c8f0820ab534d132f1312788613706732
Philip Kalinsky <philip.kalinsky@eloquentbits.com>
2023-12-29 09:04:34 -0500

    github actions build binary

--- patch-uwsgi-packaging.sh
+++ patch-uwsgi-packaging.sh
@@ -5,7 +5,8 @@ set -euf -o pipefail
 SRC=$1
 APPEND_VERSION=${APPEND_VERSION:-""}
 
-cp setup.py "${SRC}"/setup.py
+cp setup.pyuwsgi.py "${SRC}"/setup.py
+cp pyproject.pyuwsgi.toml "${SRC}"/pyproject.toml
 cp buildconf/pikesquares.ini "${SRC}"/buildconf/
 sed -i.bak 's/uwsgiconfig\.uwsgi_version/uwsgiconfig.uwsgi_version + "'$APPEND_VERSION'"/' "${SRC}"/setup.py
 rm "${SRC}"/setup.py.bak
